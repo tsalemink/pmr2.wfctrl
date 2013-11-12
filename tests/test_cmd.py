6d41595afa79b9e1fd653af0ba2093d1afadf4d6
Tommy Yu <tommy.yu@auckland.ac.nz>
2013-11-12 22:49:01 +1300

    Generalized the interaction tests a bit.
    - For reusability.

--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -15,8 +15,56 @@ def fail(*a, **kw):
     raise Exception()
 
 
+class RawCmdTests(object):
+
+    marker = None
+    cmdcls = None
+
+    def test_init(self):
+        self.cmd.init_new(self.workspace)
+        self.assertTrue(isdir(join(self.workspace_dir, self.marker)))
+
+    def test_clone(self):
+        self.cmd.init_new(self.workspace)
+        target = os.path.join(self.working_dir, 'new_target')
+        # make a new workspace, currently unknown marker (vcs backend),
+        # but soon to become git (see using git's cmd_table)
+        workspace = CmdWorkspace(target, self.marker,
+            cmd_table=self.cmd.cmd_table)
+        cmd = self.cmdcls(remote=self.workspace_dir)
+        cmd.clone(workspace)
+        self.assertTrue(isdir(join(target, self.marker)))
+
+    def test_clone_with_fresh_workspace(self):
+        self.cmd.init_new(self.workspace)
+        # XXX allow later specification of cmd_tables
+        target = os.path.join(self.working_dir, 'new_target')
+        cmdtable = self.cmd.cmd_table
+        cmdtable['init'] = fail
+        workspace = CmdWorkspace(target, self.marker,
+            cmd_table=self.cmd.cmd_table)
+        # new workspace got cloned.
+        self.assertTrue(isdir(join(target, self.marker)))
+        # XXX verify contents.
+
+    def test_add_files(self):
+        self.cmd.init_new(self.workspace)
+        helper = CoreTests()
+        helper.workspace_dir = self.workspace_dir
+        files = helper.add_files_multi(self.workspace)
+        message = 'initial commit'
+        self.workspace.save(message=message)
+        self.check_commit(message, files)
+
+    def check_commit(self,  message, files):
+        raise NotImplementedError
+
+
 @skipIf(not GitDvcsCmd.available(), 'git is not available')
-class GitDvcsCmdTestCase(CoreTestCase):
+class GitDvcsCmdTestCase(CoreTestCase, RawCmdTests):
+
+    marker = '.git'
+    cmdcls = GitDvcsCmd
 
     def setUp(self):
         super(GitDvcsCmdTestCase, self).setUp()
@@ -24,41 +72,10 @@ class GitDvcsCmdTestCase(CoreTestCase):
         self.workspace = CmdWorkspace(self.workspace_dir, self.cmd.marker,
             cmd_table=self.cmd.cmd_table)
 
-    def test_git_init(self):
-        self.cmd.init_new(self.workspace)
-        self.assertTrue(isdir(join(self.workspace_dir, '.git')))
-
-    def test_git_clone(self):
-        self.cmd.init_new(self.workspace)
-        target = os.path.join(self.working_dir, 'new_target')
-        # make a new workspace, currently unknown marker (vcs backend),
-        # but soon to become git (see using git's cmd_table)
-        workspace = CmdWorkspace(target, '.git', cmd_table=self.cmd.cmd_table)
-        cmd = GitDvcsCmd(remote=self.workspace_dir)
-        cmd.clone(workspace)
-        self.assertTrue(isdir(join(target, '.git')))
-
-    def test_git_clone_with_fresh_workspace(self):
-        self.cmd.init_new(self.workspace)
-        # XXX allow later specification of cmd_tables
-        target = os.path.join(self.working_dir, 'new_target')
-        cmdtable = self.cmd.cmd_table
-        cmdtable['init'] = fail
-        workspace = CmdWorkspace(target, '.git', cmd_table=self.cmd.cmd_table)
-        # new workspace got cloned.
-        self.assertTrue(isdir(join(target, '.git')))
-        # XXX verify contents.
-
-    def test_git_add_files(self):
-        self.cmd.init_new(self.workspace)
-        helper = CoreTests()
-        helper.workspace_dir = self.workspace_dir
-        files = helper.add_files_multi(self.workspace)
-        self.workspace.save(message='initial commit')
-
+    def check_commit(self, message, files):
         stdout, stderr = GitDvcsCmd._execute(
             self.cmd._args(self.workspace, 'log'))
-        self.assertTrue('initial commit' in stdout)
+        self.assertTrue(message in stdout)
 
         stdout, stderr = GitDvcsCmd._execute(
             self.cmd._args(self.workspace, 'ls-tree', 'master'))
