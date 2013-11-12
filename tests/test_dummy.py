75096c7445a2917c3f719a5d65834d747c9ebf2b
Tommy Yu <tommy.yu@auckland.ac.nz>
2013-11-12 20:47:56 +1300

    Split out the cmd related tests related to dummy

---
+++ tests/test_dummy.py
@@ -0,0 +1,68 @@
+from unittest import TestCase
+
+import os
+from os.path import join
+import tempfile
+import shutil
+
+from pmr.wfctrl.core import CmdWorkspace
+from pmr.wfctrl.cmd import DemoDvcsCmd
+
+from pmr.wfctrl.testing.base import CoreTestCase
+from pmr.wfctrl.testing.base import CoreTests
+
+
+class DemoVcsCmdWorkspaceTestCase(CoreTestCase, CoreTests):
+
+    wks_marker = '.marker'
+
+    def make_workspace(self):
+        self.cmd = DemoDvcsCmd()
+        os.mkdir(join(self.workspace_dir, self.wks_marker))
+        return CmdWorkspace(self.workspace_dir, self.wks_marker,
+            cmd_table=self.cmd.cmd_table)
+
+    def test_cmd_new_init(self):
+        self.cmd = DemoDvcsCmd()
+        wks = CmdWorkspace(self.workspace_dir, self.wks_marker,
+            cmd_table=self.cmd.cmd_table)
+        self.assertEqual(self.cmd.queue, [
+            ['vcs', 'init', self.workspace_dir],
+        ])
+
+    def test_cmd_new_clone(self):
+        remote = 'http://example.com'
+        self.cmd = DemoDvcsCmd(remote=remote)
+        wks = CmdWorkspace(self.workspace_dir, self.wks_marker,
+            cmd_table=self.cmd.cmd_table)
+        self.assertEqual(self.cmd.queue, [
+            ['vcs', 'clone', remote, self.workspace_dir],
+        ])
+
+        # emulate that creation, which our mocks don't do.
+        os.mkdir(join(self.workspace_dir, self.wks_marker))
+        self.cmd = DemoDvcsCmd(remote=remote)
+        wks = CmdWorkspace(self.workspace_dir, self.wks_marker,
+            cmd_table=self.cmd.cmd_table)
+        # nothing.
+        self.assertEqual(self.cmd.queue, [])
+
+    def test_cmd_add_files_simple(self):
+        wks = self.make_workspace()
+        # since we already have the marker, no extra command.
+        # ... at least until we decide to pull.
+        self.assertEqual(self.cmd.queue, [])
+
+        filename = self.add_files_simple(wks)
+        wks.save()
+        self.assertEqual(self.cmd.queue, [
+            ['vcs', 'add', filename],
+            ['vcs', 'commit', '-m', ''],
+            ['vcs', 'push'],
+        ])
+
+    def test_add_files_multi(self):
+        CoreTests.test_add_files_multi(self)
+
+    def test_add_files_nested(self):
+        CoreTests.test_add_files_nested(self)
