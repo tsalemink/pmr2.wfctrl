ab0e2b15d089b3ff8d92600e9d6612f40b328bbb
Tommy Yu <tommy.yu@auckland.ac.nz>
2013-11-14 13:59:35 +1300

    Make the username/password optional
    - Thus make it possible to extract a credential-less URL.

--- src/pmr/wfctrl/utils.py
+++ src/pmr/wfctrl/utils.py
@@ -1,12 +1,16 @@
 from urlparse import urlsplit, urlunsplit
 
-def set_url_cred(url, username, password, _protocols=('http', 'https')):
+def set_url_cred(url, username=None, password=None,
+        _protocols=('http', 'https')):
     urlparts = list(urlsplit(url))
     if urlparts[0] not in _protocols:
         return url
 
     if '@' in urlparts[1]:
         urlparts[1] = urlparts[1].split('@')[-1]
+
+    if username is None or password is None:
+        return urlunsplit(urlparts)
 
     urlparts[1] = '%s:%s@%s' % (username, password, urlparts[1])
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,6 +5,16 @@ from pmr.wfctrl import utils
 
 class SetUrlCredsTestCase(TestCase):
 
+    def test_set_none(self):
+       r = utils.set_url_cred('http://example.com')
+       self.assertEqual(r, 'http://example.com')
+       r = utils.set_url_cred('http://example.com', 'user')
+       self.assertEqual(r, 'http://example.com')
+       r = utils.set_url_cred('http://username@example.com', 'user')
+       self.assertEqual(r, 'http://example.com')
+       r = utils.set_url_cred('http://no:body@example.com', password='pass')
+       self.assertEqual(r, 'http://example.com')
+
     def test_set_new(self):
        r = utils.set_url_cred('http://example.com', 'user', 'pass')
        self.assertEqual(r, 'http://user:pass@example.com')
@@ -17,10 +27,10 @@ class SetUrlCredsTestCase(TestCase):
        r = utils.set_url_cred('http://no:body@example.com', 'user', 'pass')
        self.assertEqual(r, 'http://user:pass@example.com')
 
-    def test_not_url(self):
+    def test_posix_abs_path(self):
        r = utils.set_url_cred('/tmp/filepath', 'user', 'pass')
        self.assertEqual(r, '/tmp/filepath')
 
-    def test_windows_path(self):
+    def test_windows_abs_path(self):
        r = utils.set_url_cred('C:\\User\\Tester\\Documents', 'user', 'pass')
        self.assertEqual(r, 'C:\\User\\Tester\\Documents')
