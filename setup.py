926563f80a213260eecfb33815af3915e3fb8c2a
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-09-08 12:07:36 +1200

    Update setup.py
    Make pip install requirements from requirements.txt file.

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import sys
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 import os
+
+
+SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Must keep these two versions in sync.
 # from pmr2 import wfctrl
@@ -13,6 +17,16 @@ long_description = (
     open(os.path.join('docs', 'CHANGES.rst')).read()
     + '\n')
 
+
+class InstallCommand(install):
+
+    def run(self):
+        install.run(self)
+        # Automatically install requirements from requirements.txt
+        import subprocess
+        subprocess.call(['pip', 'install', '-r', os.path.join(SETUP_DIR, 'requirements.txt')])
+
+
 setup(name='pmr2.wfctrl',
       version=version,
       description="Workflow controller",
@@ -22,6 +36,7 @@ setup(name='pmr2.wfctrl',
       classifiers=[
         "Programming Language :: Python",
         ],
+      cmdclass={'install': InstallCommand,},
       keywords='',
       author='',
       author_email='',
