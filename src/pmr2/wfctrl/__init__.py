d38f6a0d6d7ed3c05c82f335da3847dbde86c030
Tommy Yu <tommy.yu@auckland.ac.nz>
2023-10-16 18:11:23 +1300

    Don't need to check that non-installed condition.

--- src/pmr2/wfctrl/__init__.py
+++ src/pmr2/wfctrl/__init__.py
@@ -2,5 +2,5 @@ try:
     from pkg_resources import Requirement
     from pkg_resources import working_set
     __version__ = working_set.find(Requirement.parse('pmr2.wfctrl')).version
-except Exception:
+except Exception:  # pragma: no cover
     __version__ = "unknown"
